e84d28a540d179f04d19a1ae12bd600fd5b42538
FirEmerald <mccaslinwalt@gmail.com>
2025-12-02 10:45:30 -0600

    Config fixes
    Modify BOConfig to use the same format as BOServerConfig
    Move config options that need to be synchronized to client to BOServerConfig
    Increase maximum freezing time to 5 seconds

--- src/main/java/com/sierravanguard/beyond_oxygen/BOConfig.java
+++ src/main/java/com/sierravanguard/beyond_oxygen/BOConfig.java
@@ -1,69 +1,107 @@
     package com.sierravanguard.beyond_oxygen;
 
-    import net.minecraft.resources.ResourceLocation;
     import net.minecraftforge.common.ForgeConfigSpec;
     import net.minecraftforge.eventbus.api.SubscribeEvent;
     import net.minecraftforge.fml.common.Mod;
     import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-    import java.util.ArrayList;
-    import java.util.List;
-
     @Mod.EventBusSubscriber(modid = BeyondOxygen.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public class BOConfig {
-        private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-        public static final ForgeConfigSpec.ConfigValue<Integer> VENT_RANGE = BUILDER.comment("Max range of vent, high value CAN AND WILL cause lag.").define("ventRange", 2048);
-        public static final ForgeConfigSpec.ConfigValue<Integer> OXYGEN_TANK_CAPACITY = BUILDER.comment("Max amount of oxygen that oxygen tank can contain (in mb)").define("oxygenTankCapacity", 1200);
-        public static final ForgeConfigSpec.ConfigValue<Integer> OXYGEN_CONSUMPTION = BUILDER.comment("How many oxygen units in 1 mb").define("oxygenConsumption", 10);
-        public static final ForgeConfigSpec.ConfigValue<Integer> VENT_CONSUMPTION = BUILDER.comment("How many blocks vent can fill with 1 oxygen unit").define("ventConsumption", 20);
+        private static final ForgeConfigSpec.ConfigValue<Integer> VENT_RANGE;
+        private static final ForgeConfigSpec.ConfigValue<Integer> OXYGEN_CONSUMPTION;
 
-        public static final ForgeConfigSpec.ConfigValue<Integer> BUBBLE_MAX_RADIUS = BUILDER
-                .comment("Maximum radius of bubble generators")
-                .defineInRange("bubbleMaxRadius", 5, 5, 20);
-        public static final ForgeConfigSpec.ConfigValue<Integer> TIME_TO_IMPLODE = BUILDER.comment("How many ticks (1 second ~ 20 ticks) for someone to IMPLODE from lack of air? Do not set lower than 15, or you may experience flicker.").define("timeToImplode", 10);
+        private static final ForgeConfigSpec.ConfigValue<Integer> BUBBLE_MAX_RADIUS;
+        private static final ForgeConfigSpec.ConfigValue<Integer> TIME_TO_IMPLODE;
 
-        public static final ForgeConfigSpec.BooleanValue UNBNREATHABLE_DIMENSIONS = BUILDER
-                .comment("Enable Beyond Oxygen unbreathable dimensions")
-                .define("unbreathable_dimensions", true);
-        public static final ForgeConfigSpec.BooleanValue COLD_DIMENSIONS = BUILDER
-                .comment("Enable Beyond Oxygen cold dimensions")
-                .define("cold_dimensions", true);
-        public static final ForgeConfigSpec.BooleanValue HOT_DIMENSIONS = BUILDER
-                .comment("Enable Beyond Oxygen hot dimensions")
-                .define("hot_dimensions", true);
+        private static final ForgeConfigSpec.BooleanValue UNBREATHABLE_DIMENSIONS;
+        private static final ForgeConfigSpec.BooleanValue COLD_DIMENSIONS;
+        private static final ForgeConfigSpec.BooleanValue HOT_DIMENSIONS;
 
-        static final ForgeConfigSpec SPEC = BUILDER.build();
-        private static ResourceLocation toResourceLocation(String id) {
-            String[] parts = id.split(":");
-            return new ResourceLocation(parts[0], parts[1]);
+        static final ForgeConfigSpec SPEC;
+
+        //we instantiate all config in the class constructor here so that the builder can be released from memory after creating the config spec.
+        static {
+            ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+            VENT_RANGE = builder
+                    .comment("Max range of vent, high value CAN AND WILL cause lag.")
+                    .define("ventRange", 2048);
+            OXYGEN_CONSUMPTION = builder
+                    .comment("How many oxygen units in 1 mb")
+                    .define("oxygenConsumption", 10);
+
+            BUBBLE_MAX_RADIUS = builder
+                    .comment("Maximum radius of bubble generators")
+                    .defineInRange("bubbleMaxRadius", 5, 5, 20);
+            TIME_TO_IMPLODE = builder
+                    .comment("How many ticks (1 second ~ 20 ticks) for someone to IMPLODE from lack of air? Do not set lower than 15, or you may experience flicker.")
+                    .define("timeToImplode", 10);
+
+            UNBREATHABLE_DIMENSIONS = builder
+                    .comment("Enable Beyond Oxygen unbreathable dimensions")
+                    .define("unbreathable_dimensions", true);
+            COLD_DIMENSIONS = builder
+                    .comment("Enable Beyond Oxygen cold dimensions")
+                    .define("cold_dimensions", true);
+            HOT_DIMENSIONS = builder
+                    .comment("Enable Beyond Oxygen hot dimensions")
+                    .define("hot_dimensions", true);
+
+            SPEC = builder.build();
         }
 
-        private static List<ResourceLocation> toResourceLocationList(List<? extends String> list) {
-            List<ResourceLocation> result = new ArrayList<>();
-            for (String s : list) result.add(toResourceLocation(s));
-            return result;
+        @SubscribeEvent
+        static void onLoad(final ModConfigEvent event) {
+            if (event.getConfig().getSpec() == SPEC && SPEC.isLoaded()) loadConfig();
         }
 
+        private static int ventRange;
+        private static int oxygenConsumption;
 
-        public static int getOxygenTankCapacity() {
-            return OXYGEN_TANK_CAPACITY.get();
+        private static int bubbleMaxRadius;
+        private static int timeToImplode;
+
+        private static boolean enableUnbreathableDimensions;
+        private static boolean enableColdDimensions;
+        private static boolean enableHotDimensions;
+
+        //we load all of the config values into more friendly versions here, and can process custom syntax issues.
+        public static void loadConfig() {
+            ventRange = VENT_RANGE.get();
+            oxygenConsumption = OXYGEN_CONSUMPTION.get();
+
+            bubbleMaxRadius = BUBBLE_MAX_RADIUS.get();
+            timeToImplode = TIME_TO_IMPLODE.get();
+
+            enableUnbreathableDimensions = UNBREATHABLE_DIMENSIONS.get();
+            enableColdDimensions = COLD_DIMENSIONS.get();
+            enableHotDimensions = HOT_DIMENSIONS.get();
         }
 
         public static int getVentRange() {
-            return VENT_RANGE.get();
+            return ventRange;
         }
 
         public static int getOxygenConsumption() {
-            return OXYGEN_CONSUMPTION.get();
+            return oxygenConsumption;
         }
 
         public static int getTimeToImplode() {
-            return TIME_TO_IMPLODE.get();
+            return timeToImplode;
         }
 
-        public static int getVentConsumption() {
-            return VENT_CONSUMPTION.get();
+        public static int getBubbleMaxRadius() {
+            return bubbleMaxRadius;
         }
 
-        public static int getBubbleMaxRadius(){return BUBBLE_MAX_RADIUS.get();}
+        public static boolean enableUnbreathableDimensions() {
+            return enableUnbreathableDimensions;
+        }
+
+        public static boolean enableColdDimensions() {
+            return enableColdDimensions;
+        }
+
+        public static boolean enableHotDimensions() {
+            return enableHotDimensions;
+        }
     }
--- src/main/java/com/sierravanguard/beyond_oxygen/BOServerConfig.java
+++ src/main/java/com/sierravanguard/beyond_oxygen/BOServerConfig.java
@@ -16,9 +16,13 @@ import java.util.*;
 //config values that should be automatically synchronized to the client.
 @Mod.EventBusSubscriber(modid = BeyondOxygen.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BOServerConfig {
-    private static final ForgeConfigSpec.IntValue HELD_ITEMS_OXYGEN_PRIORITY;
-    private static final ForgeConfigSpec.IntValue INVENTORY_OXYGEN_PRIORITY;
-    private static final ForgeConfigSpec.IntValue ARMOR_OXYGEN_PRIORITY;
+    private static final ForgeConfigSpec.ConfigValue<Integer> HELD_ITEMS_OXYGEN_PRIORITY;
+    private static final ForgeConfigSpec.ConfigValue<Integer> INVENTORY_OXYGEN_PRIORITY;
+    private static final ForgeConfigSpec.ConfigValue<Integer> ARMOR_OXYGEN_PRIORITY;
+
+    private static final ForgeConfigSpec.ConfigValue<Integer> OXYGEN_TANK_CAPACITY;
+    private static final ForgeConfigSpec.ConfigValue<Integer> VENT_CONSUMPTION;
+
     static final ForgeConfigSpec SPEC;
 
     //we instantiate all config in the class constructor here so that the builder can be released from memory after creating the config spec.
@@ -26,13 +30,21 @@ public class BOServerConfig {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         HELD_ITEMS_OXYGEN_PRIORITY = builder
                 .comment("Priority for consuming oxygen from held items. Use " + Integer.MIN_VALUE + " to disable.")
-                .defineInRange("heldItemsOxygenPriority", BOOxygenSources.DEFAULT_PRIORITY_HELD_ITEMS, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                .define("heldItemsOxygenPriority", BOOxygenSources.DEFAULT_PRIORITY_HELD_ITEMS);
         INVENTORY_OXYGEN_PRIORITY = builder
                 .comment("Priority for consuming oxygen from items in the player inventory. Use " + Integer.MIN_VALUE + " to disable.")
-                .defineInRange("playerInventoryOxygenPriority", BOOxygenSources.DEFAULT_PRIORITY_PLAYER_INVENTORY, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                .define("playerInventoryOxygenPriority", BOOxygenSources.DEFAULT_PRIORITY_PLAYER_INVENTORY);
         ARMOR_OXYGEN_PRIORITY = builder
                 .comment("Priority for consuming oxygen from armor items. Use " + Integer.MIN_VALUE + " to disable.")
-                .defineInRange("armorOxygenPriority", BOOxygenSources.DEFAULT_PRIORITY_ARMOR, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                .define("armorOxygenPriority", BOOxygenSources.DEFAULT_PRIORITY_ARMOR);
+
+        OXYGEN_TANK_CAPACITY = builder
+                .comment("Max amount of oxygen that oxygen tank can contain (in mb)")
+                .define("oxygenTankCapacity", 1200);
+        VENT_CONSUMPTION = builder
+                .comment("How many blocks vent can fill with 1 oxygen unit")
+                .define("ventConsumption", 20);
+
         SPEC = builder.build();
     }
 
@@ -41,10 +53,24 @@ public class BOServerConfig {
         if (event.getConfig().getSpec() == SPEC && SPEC.isLoaded()) loadConfig();
     }
 
+    private static int oxygenTankCapacity;
+    private static int ventConsumption;
+
     //we load all of the config values into more friendly versions here, and can process custom syntax issues.
     public static void loadConfig() {
         BOOxygenSources.HELD_ITEMS.get().setPriority(HELD_ITEMS_OXYGEN_PRIORITY.get());
         BOOxygenSources.INVENTORY.get().setPriority(INVENTORY_OXYGEN_PRIORITY.get());
         BOOxygenSources.ARMOR.get().setPriority(ARMOR_OXYGEN_PRIORITY.get());
+
+        oxygenTankCapacity = OXYGEN_TANK_CAPACITY.get();
+        ventConsumption = VENT_CONSUMPTION.get();
+    }
+
+    public static int getOxygenTankCapacity() {
+        return oxygenTankCapacity;
+    }
+
+    public static int getVentConsumption() {
+        return ventConsumption;
     }
 }
